ed09396492ca8c7cb392c3e00992bd2e64f78faf
dereinion <deideron@protonmail.com>
2020-06-20 21:07:05 +0300

    Game window created

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import pygame as pg
+
+# Initialize Pygame
+pg.init()
+
+# Create a window
+screen = pg.display.set_mode((800, 640))
+
+# Game loop
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
